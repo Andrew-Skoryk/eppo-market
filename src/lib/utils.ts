b8550a8d0e7a4ec978bbe0e9919b974def32e641
Andrew Skoryk <andrewtimelessone@gmail.com>
2023-10-12 14:45:04 +0300

    feat: getMoneyFormat() to avoid server currency diff error

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -13,3 +13,18 @@ export function getMoneyFormat(input: number) {
     currencyDisplay: "symbol",
   }).format(input);
 }
+
+// функція щоб не було помилки на стороні сервера
+
+// export function getMoneyFormat(input: number) {
+//   const formatted = new Intl.NumberFormat('uk-UA', {
+//     style: 'currency',
+//     currency: 'UAH',
+//     maximumFractionDigits: 0,
+//   }).format(input);
+
+//   if (formatted.endsWith('₴')) {
+//     return formatted.replace('₴', 'грн').trim();
+//   }
+//   return formatted;
+// }
