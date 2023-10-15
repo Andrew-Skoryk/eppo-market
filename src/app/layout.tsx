4ef311a84e9e027e76557daa8e102e73397867fe
Andrew Skoryk <andrewtimelessone@gmail.com>
2023-10-15 15:13:42 +0300

    change: project font from Inter to Lora

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Lora } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ukUA } from "@clerk/localizations";
 import { cn } from "../lib/utils";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import ReduxProvider from "@/components/ReduxProvider";
+import ReduxProvider from "../components/ReduxProvider";
 
-const inter = Inter({ subsets: ["latin", "cyrillic"] });
+const lora = Lora({ subsets: ["latin", "cyrillic"] });
 
 export const metadata: Metadata = {
   title: "Eppo",
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <ClerkProvider localization={ukUA}>
       <ReduxProvider>
-        <html lang="uk" className={cn("antialiased", inter.className)}>
+        <html lang="uk" className={cn("antialiased", lora.className)}>
           <body className="flex flex-col min-h-screen antialiased bg-slate-50">
             <Header />
             <main className="container flex-grow p-4 py-20 mx-auto max-w-7xl">
