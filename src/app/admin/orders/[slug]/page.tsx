99f3cee26e3499b27cc5d6f2e961738299291f2a
Andrew Skoryk <andrewtimelessone@gmail.com>
2023-10-30 13:19:41 +0200

    feat: admin order print button

--- src/app/admin/orders/[slug]/page.tsx
+++ src/app/admin/orders/[slug]/page.tsx
@@ -1,8 +1,8 @@
 import { ordersTest } from "@/components/OrdersTable/ordersTest";
 import OrderCard from "@/components/OrderCard";
-import Headings from "@/components/UI/Headings";
+import Buttons from "@/components/UI/Buttons";
 
-function OrderAdminDetailsPage({ params }: { params: { slug: string }}) {
+function OrderAdminDetailsPage({ params }: { params: { slug: string } }) {
   const askedOrder = ordersTest.find(order => order.id === +params.slug);
 
   if (!askedOrder) {
@@ -16,11 +16,11 @@ function OrderAdminDetailsPage({ params }: { params: { slug: string }}) {
   }
 
   return (
-    <section className="flex flex-col items-center space-y-3">
-      <Headings level={3}>
-        Тут буде детальна інформація по замовленню №:{" "}
-        <span className="text-3xl text-amber-600">{params.slug}</span>
-      </Headings>
+    <section className="flex flex-col space-y-4">
+      <Buttons className="self-end py-6 text-lg font-semibold">
+        Роздрукувати Замовлення
+      </Buttons>
+
       <OrderCard order={askedOrder} />
     </section>
   );
