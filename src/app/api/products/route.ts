93f34ea4a1fd6d278f4d1b652c8643e605ab4bb8
Andrew Skoryk <andrewtimelessone@gmail.com>
2023-11-18 14:17:00 +0200

    change: logs for testing db connection

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
+    console.log("Hi first");
     const products = await db.product.findMany({
     select: {
       id: true,
@@ -15,9 +16,12 @@ export async function GET() {
     }
     });
 
+        console.log("Hi second");
+
+
     return NextResponse.json({ products }, { status: 200 })
   } catch (error) {
     console.log(error);
-     return NextResponse.json({ error }, {status: 500})
+    return NextResponse.json({ error }, {status: 500})
   }
 }
